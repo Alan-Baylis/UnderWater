0b40f98bfc9747c8c644c11ce2b2e9764e67382e
Julian Voigt <baratock@hotmail.de>
2017-04-20 21:18:52 +0200

    Added some level generation

---
+++ UnderWater/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject Target; 
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    var targetPostion = Target.transform.position;
+	    targetPostion.z = -2;
+	    transform.position = targetPostion;
+	}
+}
